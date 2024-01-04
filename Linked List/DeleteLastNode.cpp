61292ecf462d3fd07dc5db7f23f9f2bb236b91f2
hasansayeed-coder <hasansayeed791@gmail.com>
2024-01-04 15:44:19 +0600

    DOing

---
+++ Linked List/DeleteLastNode.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *prev;
+    Node *next;
+    Node() : data(0), prev(nullptr), next(nullptr) {}
+    Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+    Node(int val, Node *p, Node *n) : data(val), prev(p), next(n) {}
+};
+
+Node *deleteLastNode(Node *head)
+{
+
+    if (head == NULL || head->next == NULL)
+    {
+        return NULL;
+    }
+
+    Node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->prev->next = NULL;
+    temp->prev = NULL;
+
+    delete (temp);
+
+    return head;
+}
